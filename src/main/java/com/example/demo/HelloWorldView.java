2f82d22014eed8a8330a17b40f3db9bb049a9554
unknown <eiichi.nozaki@gmail.com>
2026-06-14 18:42:21 +0200

    Hello World

--- src/main/java/com/example/demo/HelloWorldView.java
+++ src/main/java/com/example/demo/HelloWorldView.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo; // ①
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -6,20 +6,20 @@ import com.vaadin.flow.router.Route;
 /**
  * 最もシンプルな「ハローワールド」の画面です。
  * 
- * ポイント①：クラスの定義の直前に @Route と記述して、パスを指定します。
+ * ポイント①：このクラスは、DemoApplication.java と同じパッケージ（フォルダ）に配置してください。
+ * 
+ * ポイント②：クラスの定義の直前に @Route と記述して、パスを指定します。
  * この例では http://localhost:8080/hello と指定したときに表示される画面を定義しています。
  * 
- * ポイント②：VerticalLayoutを継承してください。
+ * ポイント③：VerticalLayoutを継承してください。
  * VerticalLayoutは、縦並びに部品を並べていくための「土台」です。
  * 
- * ポイント③：add()メソッドで "こんにちは、世界！" という文字列を画面に表示しています。
- * 
- * ポイント④：このクラスは、DemoApplication.java と同じパッケージ（フォルダ）に配置してください。
+ * ポイント④：add()メソッドで "こんにちは、世界！" という文字列を画面に表示しています。
  */
 @Route("hello") // ①
 public class HelloWorldView extends VerticalLayout{ // ②
 
     public HelloWorldView(){
-        add("こんにちは、世界！"); // ②
+        add("こんにちは、世界！"); // ③
     }
 }
